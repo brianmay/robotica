da91af4fe2e04b8434792dcaa799c96cefdf963f
Brian May <brian@linuxpenguins.xyz>
2017-06-18 20:27:28 +1000

    Refactor add tasks to scheduler

--- robotica/robotica.py
+++ robotica/robotica.py
@@ -1,6 +1,8 @@
 """Main module."""
 import asyncio
 
+from apscheduler.schedulers.base import BaseScheduler
+
 from robotica.lifx import Bulbs
 
 
@@ -72,3 +74,24 @@ class Tasks:
 
     async def sleep(self) -> None:
         await self.say('It is time to go to the toilet and sleep.')
+
+    def add_tasks_to_scheduler(self, scheduler: BaseScheduler):
+        scheduler.add_job(self.wake_up, 'cron', hour="06", minute="30", day_of_week="1-5")
+        scheduler.add_job(self.wake_up_urgent, 'cron', hour="07", minute="00", day_of_week="1-5")
+        scheduler.add_job(self.eat_breakfast, 'cron', hour="07", minute="10", day_of_week="1-5")
+        scheduler.add_job(self.clean_teeth, 'cron', hour="07", minute="20", day_of_week="1-5")
+        scheduler.add_job(self.get_into_car, 'cron', hour="07", minute="25", day_of_week="1-5")
+        scheduler.add_job(self.go_to_bus, 'cron', hour="07", minute="30", day_of_week="1-5")
+        scheduler.add_job(self.save_point, 'cron', hour="17", minute="55")
+        scheduler.add_job(self.save_game, 'cron', hour="18", minute="00")
+        scheduler.add_job(self.turn_off, 'cron', hour="18", minute="05")
+        scheduler.add_job(self.pack_up, 'cron', hour="18", minute="10")
+        scheduler.add_job(self.make_lunch, 'cron', hour="18", minute="20", day_of_week="0-4")
+        scheduler.add_job(self.set_the_table, 'cron', hour="18", minute="25")
+        scheduler.add_job(self.eat_tea, 'cron', hour="18", minute="30")
+        scheduler.add_job(self.eat_dessert, 'cron', hour="19", minute="00")
+        scheduler.add_job(self.shower, 'cron', hour="19", minute="15", day_of_week="0")
+        scheduler.add_job(self.slow_down, 'cron', hour="19", minute="45")
+        scheduler.add_job(self.read_in_bed, 'cron', hour="20", minute="00")
+        scheduler.add_job(self.talk_in_bed, 'cron', hour="20", minute="15")
+        scheduler.add_job(self.sleep, 'cron', hour="20", minute="30")
