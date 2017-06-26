e91f61ae5b22c340160879c49e3cd351180be030
Brian May <brian@linuxpenguins.xyz>
2017-06-27 08:14:57 +1000

    Ability to play sound files

--- robotica/audio.py
+++ robotica/audio.py
@@ -16,16 +16,24 @@ class Audio:
         with open(config, "r") as file:
             self._config = yaml.safe_load(file)
         self._say_cmd = self._config['say_cmd']
+        self._play_cmd = self._config['play_cmd']
 
     @staticmethod
     async def execute(cmd: str, params: Dict[str, str]):
         split = [
             value.format(**params) for value in shlex.split(cmd)
         ]
-        print(split)
         process = await asyncio.create_subprocess_exec(*split)
         await process.wait()
 
     async def say(self, text: str):
+        await self.play('prefix')
         await self.execute(self._say_cmd, {'text': text})
+        await self.play('repeat')
+        await self.execute(self._say_cmd, {'text': text})
+        await self.play('postfix')
 
+    async def play(self, sound: str):
+        sound_file = self._config['sounds'][sound]
+        if sound_file:
+            await self.execute(self._play_cmd, {'file': sound_file})
