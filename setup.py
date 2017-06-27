830335441e82e801edf19da27297913ba65bbbc4
Brian May <brian@linuxpenguins.xyz>
2017-06-27 17:13:45 +1000

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='robotica',
-    version='0.1.7',
+    version='0.1.8',
     description="Robotic maid to scare innocent children",
     long_description=readme + '\n\n' + history,
     author="Brian May",
